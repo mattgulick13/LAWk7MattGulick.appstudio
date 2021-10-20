a4336614fc36c08907d6ed6e047a26eae3268fcb
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-10-20 16:48:27 -0500

    Update customerAdd.js

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -1,19 +1,18 @@
-customerSelect.onshow = function() {
-  query = "SELECT * FROM customer"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+btnSubmit.onclick=function(){
+  name = inptName.value
+  address = inptAddress.value
+  city = inptCity.value
+  state = inptState.value
+  zipcode = inptZipcode.value
+  query = "INSERT INTO customer (name,street,city,state,zipcode) VALUES ('" + name + "', '" + address + "', '" + city + "', '" + state + "', '" + zipcode + "')"
+  alert(query)
 
-  if (req.status == 200) { 
-    results = JSON.parse(req.responseText)
-    console.log(`the results are \n ${results}`)
-    if (results.length == 0)
-      lblMessage.value = "There are no customers in the database."
-    else {
-      let message = ""
-      for (i = 0; i < results.length; i++)
-        message = message + results[i][1] + "\n"
-      txtCustomerName.value = message
-    } 
-
-  } else 
-    lblMessage.value = "Error code: " + req.status
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + query)
+  if (req.status == 200) {
+    if (req.responseText == 500)
+      lbl1.value = "You have added the person!"
+    else
+      lbl1.value = "There was a problem with adding the person to the database."
+  } else
+    lbl1.value = "Error: " + req.status
 }
