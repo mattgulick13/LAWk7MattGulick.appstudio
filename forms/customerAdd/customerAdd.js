2f9854c3b16f00d6292890f08ce44a9c31d3ed3e
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-10-20 16:24:04 -0500

    Create customerAdd.js

---
+++ forms/customerAdd/customerAdd.js
@@ -0,0 +1,19 @@
+customerSelect.onshow = function() {
+  query = "SELECT * FROM customer"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+  if (req.status == 200) { 
+    results = JSON.parse(req.responseText)
+    console.log(`the results are \n ${results}`)
+    if (results.length == 0)
+      lblMessage.value = "There are no customers in the database."
+    else {
+      let message = ""
+      for (i = 0; i < results.length; i++)
+        message = message + results[i][1] + "\n"
+      txtCustomerName.value = message
+    } 
+
+  } else 
+    lblMessage.value = "Error code: " + req.status
+}
